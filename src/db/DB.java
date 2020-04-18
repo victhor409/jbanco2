01b40239ece9951d1480719f8c8bd2b599bcd7c9
victhor409 <victhor409@gmail.com>
2020-04-18 08:46:41 -0300

    JDBC2

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,26 +4,26 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
 
-	private static Connection conn=null;
+private static Connection conn = null;
 	
 	public static Connection getConnection() {
-		
 		if (conn == null) {
 			try {
-				Properties props = new Properties();
+				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
-				conn=DriverManager.getConnection(url,props);
-				}
+				conn = DriverManager.getConnection(url, props);
+			}
 			catch (SQLException e) {
 				throw new DbException(e.getMessage());
-				}
+			}
 		}
-			
 		return conn;
 	}
 	
@@ -31,17 +31,14 @@ public class DB {
 		if (conn != null) {
 			try {
 				conn.close();
-			}
-			catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-
-	
 	
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")){
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
@@ -49,11 +46,25 @@ public class DB {
 		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
-		
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
 	}
 }
-
-
-
-
-	
